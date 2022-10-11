857bd640144c70d4d3da57fa766ce257a57a333c
Ryan Kielty <rkielty@dev-10.com>
2022-10-11 09:56:21 -0500

    Found and fixed one issue for login

--- budget-controller-server/budget-controller/src/main/java/learn/budget/data/UserJdbcRepo.java
+++ budget-controller-server/budget-controller/src/main/java/learn/budget/data/UserJdbcRepo.java
@@ -19,18 +19,16 @@ public class UserJdbcRepo implements UserRepo {
         List<MyRole> userRoles = getRolesByUsername(username);
 
         return template.query(
-                "select user_id, username, passhash, email, isDeleted from app_user where username = ?",
+                "select user_id, username, passhash, email, isDeleted from appUser where username = ?",
                 new AppUserMapper(userRoles),
                 username).stream().findFirst().orElse(null);
     }
 
     private List<MyRole> getRolesByUsername(String username) {
         String sql = "select r.role_id, r.role_name\n" +
-                "from app_role as r\n" +
-                "inner join user_roles as ur\n" +
-                "\ton r.role_id = ur.role_id\n" +
-                "inner join app_user as u\n" +
-                "\ton u.user_id = ur.user_id\n" +
+                "from myRole as r\n" +
+                "inner join appUser as u\n" +
+                "\ton u.user_id = r.user_id\n" +
                 "where u.username = ?;";
 
         return template.query(sql, new MyRoleMapper(), username);
