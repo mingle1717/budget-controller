4b3e7f11b6ed3f3edfaef1b9834510fbd562b306
unknown <kendyrng@gmail.com>
2022-10-14 12:36:48 -0400

    retest

--- api/budget-controller/src/main/java/learn/budget/controllers/TransactionController.java
+++ api/budget-controller/src/main/java/learn/budget/controllers/TransactionController.java
@@ -1,9 +1,16 @@
 package learn.budget.controllers;
 
+import learn.budget.domain.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/transaction")
 public class TransactionController {
+
+    @Autowired
+    TransactionService transactionService;
+
+
 }
