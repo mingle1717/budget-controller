25e23b77c11a7f61ea8504483566655b893018bc
unknown <kendyrng@gmail.com>
2022-10-17 15:33:38 -0400

    Added AutoTriggerController

--- api/budget-controller/src/main/java/learn/budget/schedule/TransactionSchedule.java
+++ api/budget-controller/src/main/java/learn/budget/schedule/TransactionSchedule.java
@@ -9,8 +9,6 @@ import learn.budget.domain.TransactionService;
 import learn.budget.models.AutoTrigger;
 import learn.budget.models.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
 
