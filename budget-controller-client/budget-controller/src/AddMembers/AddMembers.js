50207fc712ff35cabf3e5edf948bcea97886e76e
hugo moreno <cristianmq17@gmail.com>
2022-10-18 22:03:02 -0700

    worked on adding members, set app background color

--- budget-controller-client/budget-controller/src/AddMembers/AddMembers.js
+++ budget-controller-client/budget-controller/src/AddMembers/AddMembers.js
@@ -3,14 +3,24 @@ import AddMemberContainer from './AddMemberContainer';
 import { useEffect } from "react";
 import { useContext, useState } from "react";
 import AuthContext from "../AuthContext";
+import FormInput from '../FormInput';
 function AddMembers(){
 
     const auth = useContext(AuthContext);
    
- 
+    
+    const [userToAdd, setUserToAdd] = useState({username:"", budgetId : ""})
     
 
-    
+    function memberChangeHandler(inputChangeEvent){
+        const propertyName = inputChangeEvent.target.name;
+        const newValue = inputChangeEvent.target.value;
+       const memberCopy = {...userToAdd};
+        memberCopy[propertyName] = newValue;
+         
+         setUserToAdd(memberCopy)
+         console.log(memberCopy)
+    }
     
     useEffect(() => {
         fetch("http://localhost:8080/api/budget/personal/" + auth.user.username, {
@@ -29,7 +39,10 @@ function AddMembers(){
             }
         })
         .then(budget => {
-            console.log(budget);
+
+            const userToAddCopy = {username: userToAdd.username, budgetId: budget.budgetId}
+            setUserToAdd(userToAddCopy)
+
         })
         .catch(error => {
             console.log(error);
@@ -39,7 +52,16 @@ function AddMembers(){
 
     return(
     <div>
-        <AddMemberContainer/>
+         <div className="container">
+             <FormInput 
+                    inputType={"text"} 
+                    identifier={"username"} 
+                    labelText={"Add members"} 
+                    currVal={userToAdd.username} 
+                    labelClass={"membersLabel"}
+                    onChangeHandler={memberChangeHandler}  
+                    className={"form-control"}/>    
+    </div>
     </div>
     )
 } export default AddMembers;
